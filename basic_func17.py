31b05f56007afd8a0684c56c0eb9242d2402f2ed
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-11 17:30:30 +0500

    basic_func17.py

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
+from cmath import pi
+
+
+def main(a):
+    return a
+print(main(pi))
